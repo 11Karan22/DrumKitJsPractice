6ffb19605dfa0823c3bae80dafdcc8847a437b89
Karan <kstkaranthakur03@gmail.com>
2023-11-02 22:54:10 +0530

    adding event listner on clicking

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,11 @@
+
+
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function l̥() {//using anonymmous function
+        alert("I am clicked!");
+    });
+}
+
+
+
+
