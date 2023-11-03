d64c7ec6faa079964dca0ea48e88cfd28aa7064e
Karan <kstkaranthakur03@gmail.com>
2023-11-04 01:34:14 +0530

    audio effect

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,43 @@
 
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function l̥() {//using anonymmous function
-        alert("I am clicked!");
+        // alert("I am clicked!");
+        //this object is very importantin js it returns the identity of the object or a kind of whole object on which operation is being performed
+        // this.style.color="white";
+        var buttonInnerHtml = this.innerHTML;
+        switch (buttonInnerHtml) {
+            case "w":
+                var audio = new Audio("./sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("./sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("./sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("./sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("./sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("./sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("./sounds/snare.mp3");
+                audio.play();
+                break;
+
+            default:
+                console.log("Invalid");    
+        }
     });
 }
 
